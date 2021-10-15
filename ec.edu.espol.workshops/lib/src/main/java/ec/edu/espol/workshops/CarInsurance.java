08b39c60eddc90be7ce985bcce7dd324872606c3
Hayleen Carrillo <haivcarr@espol.edu.ec>
2021-10-14 23:53:11 -0500

    Creation of getters of "CarInsurance"

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -6,4 +6,20 @@ public class CarInsurance {
 	private String maritalStatus;
 	
 	final int minimo = 1000;
+	
+	
+	public String getAge() {
+	    return age;
+	  }
+	
+	public String getSex() {
+	    return sex;
+	  }
+	
+	public String getMaritalStatus() {
+	    return maritalStatus;
+	  }
+	
 }
+
+
