c1cceca6c50d1fc752b78a38f30ea65fa9d618ae
Isaac Ponce <ispovala@outlook.com>
2021-10-18 19:58:45 -0500

    Insurance Calculator

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -1,39 +1,94 @@
 package ec.edu.espol.workshops;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CarInsurance {
-	private String age;
-	private String sex;
-	private String maritalStatus;
-	
-	final int minimo = 1000;
-	
-	
-	public String getAge() {
-	    return age;
-	  }
-	
-	public String getSex() {
-	    return sex;
-	  }
-	
-	public String getMaritalStatus() {
-	    return maritalStatus;
-	  }
-	
-	public void setAge(String age)
-    {
-            this.age = age;
-    }
-	
-	public void setSex(String sex)
-    {
-            this.sex = sex;
-    }
-	
-	public void setMaritalStatus(String maritalStatus)
-    {
-            this.maritalStatus = maritalStatus;
-    }
+
+	public class Customer {
+
+		private int age;
+		private char sex;
+		private boolean married;
+		private String driverLicense;
+
+		public int getAge() {
+			return this.age;
+		}
+
+		public char getSex() {
+			return this.sex;
+		}
+
+		public boolean isMarried() {
+			return this.married;
+		}
+
+		public String driverLicense() {
+			return this.driverLicense;
+		}
+
+		public void setAge(int age) {
+			this.age = age;
+		}
+
+		public void setSex(char sex) {
+			this.sex = sex;
+		}
+
+		public void setMarried(boolean married) {
+			this.married = married;
+		}
+
+		public void setDriverLicense(String driverLicense) {
+			this.driverLicense = driverLicense;
+		}
+	}
+
+	public static void main(String args[]) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Premium Car Insurance Calculator");
+		System.out.println("Enter customer information");
+
+		System.out.print("Age:");
+		int age = sc.nextInt();
+		sc.nextLine();
+
+		System.out.print("Sex [M/F] :");
+		char sex = sc.nextLine().charAt(0);
+
+		System.out.print("Is married? [Y/N]:");
+		boolean married = String.valueOf(sc.nextLine().charAt(0)).equalsIgnoreCase("y");
+
+		System.out.print("Driving License:");
+		String license = sc.nextLine();
+		
+		System.out.print(CarInsurance.calculateInsurance(age,sex,married,license));
+
+	}
+
+	public static int calculateInsurance(int age, char sex, boolean married, String license) {
+
+		boolean validLicense = true;
+		
+		if (age > 80 || !validLicense) {
+			return -1;
+		}
+		
+		int total = 500;
+		
+		if ((sex=='m' || sex=='M') && !married && age<25 ) {
+			total+=1500;
+		}
+		else if (sex=='f' || sex=='F' || married) {
+			total -=200;
+		}
+		if (age>45 && age<65) {
+			total -=100;
+		}
+
+		return total;
+	}
+
 }
-
-
