a6d6777c42a42f9f0b8f733ff12c84da412ff75f
Isaac Ponce <ispovala@outlook.com>
2021-10-21 19:04:55 -0500

    +20 PDM warnings solved

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -2,13 +2,36 @@ package ec.edu.espol.workshops;
 
 import java.util.Scanner;
 
+/**
+ * @author ispovala
+ *
+ */
 public class CarInsurance {
 
+	private CarInsurance() {
+	}
+
+	/**
+	 * @author ispovala
+	 *
+	 */
 	public class Customer {
 
+		/**
+		 * AGE OF THE COSTUMER
+		 */
 		private int age;
+		/**
+		 * SEX OF THE COSTUMER
+		 */
 		private char sex;
+		/**
+		 * IS THE COSTUMER MARRIED
+		 */
 		private boolean married;
+		/**
+		 * 10 DIGITS DRIVER LICENSE OF THE COSTUMER
+		 */
 		private String driverLicense;
 
 		public int getAge() {
@@ -23,7 +46,7 @@ public class CarInsurance {
 			return this.married;
 		}
 
-		public String driverLicense() {
+		public String getDriverLicense() {
 			return this.driverLicense;
 		}
 
@@ -44,13 +67,13 @@ public class CarInsurance {
 		}
 	}
 
+	/**
+	 * @param args
+	 */
 	public static void main(String args[]) {
 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Premium Car Insurance Calculator");
-		System.out.println("Enter customer information");
-
-		System.out.print("Age:");
+		System.out.print("Premium Car Insurance Calculator\n" + "Enter customer information\n" + "Age:");
 		String age = sc.nextLine();
 		boolean validAge = isNumeric(age);
 		while (!validAge) {
@@ -58,32 +81,30 @@ public class CarInsurance {
 			age = sc.nextLine();
 			validAge = isNumeric(age);
 		}
-		if (Integer.valueOf(age) > 80) {
+		boolean isOlder = Integer.valueOf(age) > 80;
+		if (isOlder) {
 			System.out.print("sorry, premium car insurance is not available for 80 years older");
 			return;
 		}
-		
 
 		System.out.print("Sex [M/F] :");
 		String sex = sc.nextLine();
-		boolean validSex = sex.equalsIgnoreCase("M") || sex.equalsIgnoreCase("F");
+		boolean validSex = "M".equalsIgnoreCase(sex) || "F".equalsIgnoreCase(sex);
 		while (!validSex) {
 			System.out.print("Enter an correct sex [M/F]:");
-			sex = sc.nextLine();		
-			validSex = sex.equalsIgnoreCase("M") || sex.equalsIgnoreCase("F");
+			sex = sc.nextLine();
+			validSex = "M".equalsIgnoreCase(sex) || "F".equalsIgnoreCase(sex);
 		}
-		
 
 		System.out.print("Is married? [Y/N]:");
 		String yOrN = sc.nextLine();
-		boolean validMarried = yOrN.equalsIgnoreCase("Y") || yOrN.equalsIgnoreCase("N");
+		boolean validMarried = "Y".equalsIgnoreCase(yOrN) || "N".equalsIgnoreCase(yOrN);
 		while (!validMarried) {
 			System.out.print("Enter an correct answer for married [Y/N]:");
-			yOrN =  sc.nextLine();
-			validMarried = yOrN.equalsIgnoreCase("Y") || yOrN.equalsIgnoreCase("N");
+			yOrN = sc.nextLine();
+			validMarried = "Y".equalsIgnoreCase(yOrN) || "N".equalsIgnoreCase(yOrN);
 		}
-		boolean married =  yOrN.equalsIgnoreCase("Y");
-		
+		boolean married = "Y".equalsIgnoreCase(yOrN);
 
 		System.out.print("Driving License [ten digits]:");
 		String license = sc.nextLine();
@@ -93,18 +114,21 @@ public class CarInsurance {
 			license = sc.nextLine();
 			validLicense = isNumeric(license) && license.length() == 10;
 		}
-		
+		sc.close();
 
 		int total = 500;
-		if (sex.equalsIgnoreCase("m") && !married && Integer.valueOf(age) < 25) {
+		boolean isYounger = Integer.valueOf(age) < 25;
+		boolean isMale = "M".equalsIgnoreCase(sex);
+		if (isMale && !married && isYounger) {
 			total += 1500;
-		} else if (sex.equalsIgnoreCase("f") || married) {
+		} else if (!isMale || married) {
 			total -= 200;
 		}
 		if (Integer.valueOf(age) > 45 && Integer.valueOf(age) < 65) {
 			total -= 100;
 		}
 		System.out.print(total);
+
 
 	}
 
