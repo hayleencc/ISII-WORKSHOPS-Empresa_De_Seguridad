3420192575ffaca549912b57132c8acbbea979d6
ispovala <91394565+ispovala@users.noreply.github.com>
2021-10-29 21:28:36 -0500

    LAST VERSION BEFORE TEST
    This commit include the merge of the other branches and its corrections.

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -1,177 +1,144 @@
 package ec.edu.espol.workshops;
-
 import java.util.Scanner;
 
 /**
  * @author ispovala
  *
-
- */
-//Clase principal CarInsurance
-public class CarInsurance {
-/* Subclase Customer que contiene los 4 atributos requeridos
- * la edad, sexo, si esta casado o no, y el numero de licencia.
-
-
  */
 public class CarInsurance {
 
-	static class Customer {
+    private CarInsurance() {
+    }
 
-		/**
-		 * AGE OF THE COSTUMER
-		 */
-		private int age;
-		/**
-		 * SEX OF THE COSTUMER
-		 */
-		private char sex;
-		/**
-		 * IS THE COSTUMER MARRIED
-		 */
-		private boolean married;
-		/**
-		 * 10 DIGITS DRIVER LICENSE OF THE COSTUMER
-		 */
-		private String driverLicense;
+    /**
+     * @author ispovala
+     *
+     */
+    public class Customer {
 
-		public int getAge() {
-			return this.age;
-		}
+        /**
+         * AGE OF THE COSTUMER
+         */
+        private int age;
+        /**
+         * SEX OF THE COSTUMER
+         */
+        private char sex;
+        /**
+         * IS THE COSTUMER MARRIED
+         */
+        private boolean married;
+        /**
+         * 10 DIGITS DRIVER LICENSE OF THE COSTUMER
+         */
+        private String driverLicense;
 
+        public int getAge() {
+            return this.age;
+        }
 
-	private CarInsurance() {
-	}
+        public char getSex() {
+            return this.sex;
+        }
 
-	/**
-	 * @author ispovala
-	 *
-	 */
-	public class Customer {
+        public boolean isMarried() {
+            return this.married;
+        }
 
-		/**
-		 * AGE OF THE COSTUMER
-		 */
-		private int age;
-		/**
-		 * SEX OF THE COSTUMER
-		 */
-		private char sex;
-		/**
-		 * IS THE COSTUMER MARRIED
-		 */
-		private boolean married;
-		/**
-		 * 10 DIGITS DRIVER LICENSE OF THE COSTUMER
-		 */
-		private String driverLicense;
+        public String getDriverLicense() {
+            return this.driverLicense;
+        }
 
-		public int getAge() {
-			return this.age;
-		}
+        public void setAge(int age) {
+            this.age = age;
+        }
 
-		public char getSex() {
-			return this.sex;
-		}
+        public void setSex(char sex) {
+            this.sex = sex;
+        }
 
-	public boolean isMarried() {
-		return this.married;
-	}
+        public void setMarried(boolean married) {
+            this.married = married;
+        }
 
+        public void setDriverLicense(String driverLicense) {
+            this.driverLicense = driverLicense;
+        }
+    }
 
-		public String getDriverLicense() {
-			return this.driverLicense;
-		}
+    /**
+     * @param args
+     */
+    public static void main(String args[]) {
 
-		public void setAge(int age) {
-			this.age = age;
-		}
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Premium Car Insurance Calculator\n" + "Enter customer information\n" + "Age:");
+        String age = sc.nextLine();
+        boolean validAge = isNumeric(age);
+        while (!validAge) {
+            System.out.print("Enter an valid number for age:");
+            age = sc.nextLine();
+            validAge = isNumeric(age);
+        }
+        boolean isOlder = Integer.valueOf(age) > 80;
+        if (isOlder) {
+            System.out.print("sorry, premium car insurance is not available for 80 years older");
+            return;
+        }
 
-		public void setSex(char sex) {
-			this.sex = sex;
-		}
+        System.out.print("Sex [M/F] :");
+        String sex = sc.nextLine();
+        boolean validSex = "M".equalsIgnoreCase(sex) || "F".equalsIgnoreCase(sex);
+        while (!validSex) {
+            System.out.print("Enter an correct sex [M/F]:");
+            sex = sc.nextLine();
+            validSex = "M".equalsIgnoreCase(sex) || "F".equalsIgnoreCase(sex);
+        }
 
-		public void setMarried(boolean married) {
-			this.married = married;
-		}
+        System.out.print("Is married? [Y/N]:");
+        String yOrN = sc.nextLine();
+        boolean validMarried = "Y".equalsIgnoreCase(yOrN) || "N".equalsIgnoreCase(yOrN);
+        while (!validMarried) {
+            System.out.print("Enter an correct answer for married [Y/N]:");
+            yOrN = sc.nextLine();
+            validMarried = "Y".equalsIgnoreCase(yOrN) || "N".equalsIgnoreCase(yOrN);
+        }
+        boolean married = "Y".equalsIgnoreCase(yOrN);
 
-		public void setDriverLicense(String driverLicense) {
-			this.driverLicense = driverLicense;
-		}
-	}
+        System.out.print("Driving License [ten digits]:");
+        String license = sc.nextLine();
+        boolean validLicense = isNumeric(license) && license.length() == 10;
+        while (!validLicense) {
+            System.out.print("Come on, Driving License [ten digits]:");
+            license = sc.nextLine();
+            validLicense = isNumeric(license) && license.length() == 10;
+        }
+        sc.close();
 
-	/**
-	 * @param args
-	 */
-	public static void main(String args[]) {
+        int total = 500;
+        boolean isYounger = Integer.valueOf(age) < 25;
+        boolean isMale = "M".equalsIgnoreCase(sex);
+        if (isMale && !married && isYounger) {
+            total += 1500;
+        } else if (!isMale || married) {
+            total -= 200;
+        }
+        if (Integer.valueOf(age) > 45 && Integer.valueOf(age) < 65) {
+            total -= 100;
+        }
+        System.out.println("");
+        System.out.println("Premiunm Car Insurance:");
+        System.out.println("$"+total);
+        System.out.println("");
 
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Premium Car Insurance Calculator\n" + "Enter customer information\n" + "Age:");
-		String age = sc.nextLine();
-		boolean validAge = isNumeric(age);
-		while (!validAge) {
-			System.out.print("Enter an valid number for age:");
-			age = sc.nextLine();
-			validAge = isNumeric(age);
-		}
-		boolean isOlder = Integer.valueOf(age) > 80;
-		if (isOlder) {
-			System.out.print("sorry, premium car insurance is not available for 80 years older");
-			return;
-		}
+    }
 
-		System.out.print("Sex [M/F] :");
-		String sex = sc.nextLine();
-		boolean validSex = "M".equalsIgnoreCase(sex) || "F".equalsIgnoreCase(sex);
-		while (!validSex) {
-			System.out.print("Enter an correct sex [M/F]:");
-			sex = sc.nextLine();
-			validSex = "M".equalsIgnoreCase(sex) || "F".equalsIgnoreCase(sex);
-		}
-
-		System.out.print("Is married? [Y/N]:");
-		String yOrN = sc.nextLine();
-		boolean validMarried = "Y".equalsIgnoreCase(yOrN) || "N".equalsIgnoreCase(yOrN);
-		while (!validMarried) {
-			System.out.print("Enter an correct answer for married [Y/N]:");
-			yOrN = sc.nextLine();
-			validMarried = "Y".equalsIgnoreCase(yOrN) || "N".equalsIgnoreCase(yOrN);
-		}
-		boolean married = "Y".equalsIgnoreCase(yOrN);
-
-		System.out.print("Driving License [ten digits]:");
-		String license = sc.nextLine();
-		boolean validLicense = isNumeric(license) && license.length() == 10;
-		while (!validLicense) {
-			System.out.print("Come on, Driving License [ten digits]:");
-			license = sc.nextLine();
-			validLicense = isNumeric(license) && license.length() == 10;
-		}
-		sc.close();
-
-		int total = 500;
-		boolean isYounger = Integer.valueOf(age) < 25;
-		boolean isMale = "M".equalsIgnoreCase(sex);
-		if (isMale && !married && isYounger) {
-			total += 1500;
-		} else if (!isMale || married) {
-			total -= 200;
-		}
-		if (Integer.valueOf(age) > 45 && Integer.valueOf(age) < 65) {
-			total -= 100;
-		}
-		System.out.print(total);
-
-
-	}
-
-	private static boolean isNumeric(String license) {
-		try {
-			Long.parseLong(license);
-			return true;
-		} catch (NumberFormatException nfe) {
-			return false;
-		}
-	}
-
+    private static boolean isNumeric(String license) {
+        try {
+            Long.parseLong(license);
+            return true;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+    }
 }
