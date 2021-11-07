b573bb0c86eae468682008619b6a6d615780a5ea
milenortegam <milenortega_10@hotmail.com>
2021-11-06 23:07:59 -0500

    test with cucumber

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -6,8 +6,53 @@ import java.util.Scanner;
  *
  */
 public class CarInsurance {
+	private int age;
+    /**
+     * SEX OF THE COSTUMER
+     */
+    private String sex;
+    /**
+     * IS THE COSTUMER MARRIED
+     */
+    private boolean married;
+    /**
+     * 10 DIGITS DRIVER LICENSE OF THE COSTUMER
+     */
+    private String driverLicense;
 
-    private CarInsurance() {
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getSex() {
+        return this.sex;
+    }
+
+    public boolean isMarried() {
+        return this.married;
+    }
+
+    public String getDriverLicense() {
+        return this.driverLicense;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setMarried(boolean married) {
+        this.married = married;
+    }
+
+    public void setDriverLicense(String driverLicense) {
+        this.driverLicense = driverLicense;
+    }
+    
+    public CarInsurance() {
     }
 
     /**
@@ -19,51 +64,7 @@ public class CarInsurance {
         /**
          * AGE OF THE COSTUMER
          */
-        private int age;
-        /**
-         * SEX OF THE COSTUMER
-         */
-        private char sex;
-        /**
-         * IS THE COSTUMER MARRIED
-         */
-        private boolean married;
-        /**
-         * 10 DIGITS DRIVER LICENSE OF THE COSTUMER
-         */
-        private String driverLicense;
-
-        public int getAge() {
-            return this.age;
-        }
-
-        public char getSex() {
-            return this.sex;
-        }
-
-        public boolean isMarried() {
-            return this.married;
-        }
-
-        public String getDriverLicense() {
-            return this.driverLicense;
-        }
-
-        public void setAge(int age) {
-            this.age = age;
-        }
-
-        public void setSex(char sex) {
-            this.sex = sex;
-        }
-
-        public void setMarried(boolean married) {
-            this.married = married;
-        }
-
-        public void setDriverLicense(String driverLicense) {
-            this.driverLicense = driverLicense;
-        }
+        
     }
 
     /**
@@ -71,68 +72,35 @@ public class CarInsurance {
      */
     public static void main(String args[]) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Premium Car Insurance Calculator\n" + "Enter customer information\n" + "Age:");
-        String age = sc.nextLine();
-        boolean validAge = isNumeric(age);
-        while (!validAge) {
-            System.out.print("Enter an valid number for age:");
-            age = sc.nextLine();
-            validAge = isNumeric(age);
-        }
-        boolean isOlder = Integer.valueOf(age) > 80;
-        if (isOlder) {
-            System.out.print("sorry, premium car insurance is not available for 80 years older");
-            return;
-        }
-
-        System.out.print("Sex [M/F] :");
-        String sex = sc.nextLine();
-        boolean validSex = "M".equalsIgnoreCase(sex) || "F".equalsIgnoreCase(sex);
-        while (!validSex) {
-            System.out.print("Enter an correct sex [M/F]:");
-            sex = sc.nextLine();
-            validSex = "M".equalsIgnoreCase(sex) || "F".equalsIgnoreCase(sex);
-        }
-
-        System.out.print("Is married? [Y/N]:");
-        String yOrN = sc.nextLine();
-        boolean validMarried = "Y".equalsIgnoreCase(yOrN) || "N".equalsIgnoreCase(yOrN);
-        while (!validMarried) {
-            System.out.print("Enter an correct answer for married [Y/N]:");
-            yOrN = sc.nextLine();
-            validMarried = "Y".equalsIgnoreCase(yOrN) || "N".equalsIgnoreCase(yOrN);
-        }
-        boolean married = "Y".equalsIgnoreCase(yOrN);
-
-        System.out.print("Driving License [ten digits]:");
-        String license = sc.nextLine();
-        boolean validLicense = isNumeric(license) && license.length() == 10;
-        while (!validLicense) {
-            System.out.print("Come on, Driving License [ten digits]:");
-            license = sc.nextLine();
-            validLicense = isNumeric(license) && license.length() == 10;
-        }
-        sc.close();
-
-        int total = 500;
-        boolean isYounger = Integer.valueOf(age) < 25;
-        boolean isMale = "M".equalsIgnoreCase(sex);
-        if (isMale && !married && isYounger) {
-            total += 1500;
-        } else if (!isMale || married) {
-            total -= 200;
-        }
-        if (Integer.valueOf(age) >= 45 && Integer.valueOf(age) < 65) {
-            total -= 100;
-        }
-        System.out.println("");
-        System.out.println("Premium Car Insurance:");
-        System.out.println("$"+total);
-        System.out.println("");
+        
 
     }
-
+    
+    public String getPremium() {
+    	
+    	
+        boolean isOlder = Integer.valueOf(this.age) > 80;
+        
+        
+        int total = 500;
+        boolean isYounger = Integer.valueOf(this.age) < 25;
+        boolean isMale = "M".equalsIgnoreCase(this.sex);
+        if(isOlder || driverLicense.length() != 10) {
+        	return "0";
+        }
+        if (isMale && !this.married && isYounger) {
+            total += 1500;
+        } else if (!isMale || this.married) {
+            total -= 200;
+        }
+        if (Integer.valueOf(this.age) >= 45 && Integer.valueOf(this.age) < 65) {
+            total -= 100;
+        }
+        
+        return String.valueOf(total);
+    }
+    
+    
     private static boolean isNumeric(String license) {
         try {
             Long.parseLong(license);
---
+++ ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/StepsDefs.java
@@ -0,0 +1,92 @@
+package ec.edu.espol.workshops;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import static org.junit.Assert.*;
+
+
+public class StepsDefs {
+	private String actualAnswer;	
+	private int m_age=18;
+	
+	public CarInsurance carInsurance = new CarInsurance();
+	
+	
+	@Given("The customer has 21 years old, is single and female, and her license is 1904002349")
+	public void good_age() {
+		carInsurance.setAge(21);
+		carInsurance.setMarried(false);
+		carInsurance.setSex("F");
+		carInsurance.setDriverLicense("1904002349");
+	}
+		
+	@When("We Calculate the premium car insurance")
+	public void calculate_premium1() {
+		actualAnswer = carInsurance.getPremium();
+	}
+	@Then("We should receive: Premium Car Insurance: $200")
+	public void receive1() {
+		assertEquals("300", actualAnswer);
+	}
+	
+	
+		
+	@Given("The customer has 81 years old, is single and female, and her license is 1904002349")
+	public void advanced_age() {
+		carInsurance.setAge(81);
+		carInsurance.setMarried(false);
+		carInsurance.setSex("F");
+		carInsurance.setDriverLicense("1904002349");
+	}
+		
+	@When("The customer gives her age")
+	public void calculate_premium2() {
+		actualAnswer = carInsurance.getPremium();
+	}
+	@Then("We should receive: I should be told sorry, premium car insurance is not available for 80 years older ")
+	public void receive2() {
+		assertEquals("0", actualAnswer);
+	}
+	
+	
+	
+
+	@Given("The customer has 50 years old, is single and female, and her license is 190400234")
+	public void good_age3() {
+		carInsurance.setAge(50);
+		carInsurance.setMarried(false);
+		carInsurance.setSex("F");
+		carInsurance.setDriverLicense("190400234");
+	}
+		
+	@When("The customer Enter driver’s license number")
+	public void calculate_premium3() {
+		actualAnswer = carInsurance.getPremium();
+	}
+	@Then("I should be told Come on, Driving License [ten digits]: ")
+	public void receive3() {
+		assertEquals("0", actualAnswer);
+	}
+	
+	
+	
+	
+	@Given("The customer has 24 years old, is single and male, and his license is 1904002345")
+	public void good_age4() {
+		carInsurance.setAge(24);
+		carInsurance.setMarried(false);
+		carInsurance.setSex("M");
+		carInsurance.setDriverLicense("1904002345");
+	}
+		
+	@When("The customer Calculates the premium car insurance")
+	public void calculate_premium4() {
+		actualAnswer = carInsurance.getPremium();
+	}
+	@Then("I should be told Premium Car Insurance: $2000 ")
+	public void receive4() {
+		assertEquals("2000", actualAnswer);
+	}
+
+}
