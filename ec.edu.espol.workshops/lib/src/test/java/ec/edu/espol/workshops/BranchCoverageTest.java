0b7c168cb528823fe0ab128e57275238cf4187bc
Hayleen <haivcarr@espol.edu.ec>
2022-01-03 21:56:40 -0500

    Branch Coverage 2

--- ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/BranchCoverageTest.java
+++ ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/BranchCoverageTest.java
@@ -1,18 +1,15 @@
 package ec.edu.espol.workshops;
-
+import org.junit.Assert;
+import org.junit.Test;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
-
-import org.junit.Assert;
-import org.junit.Test;
-
-public class PathCoverageTest {
+public class BranchCoverageTest {
 	@Test
-    public void IP_001(){
+    public void BC001(){
        InputStream stdin = System.in;
-       System.setIn(new ByteArrayInputStream("21\nF\nY\n1234567890\n".getBytes()));
+       System.setIn(new ByteArrayInputStream("none\n48\nund\nF\n*\nY\nnum\n1234567891\n".getBytes()));
 
        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
        PrintStream ps = new PrintStream(byteArrayOutputStream);
@@ -26,197 +23,91 @@ public class PathCoverageTest {
 
        String outputText = byteArrayOutputStream.toString();
        String[] arr = outputText.split("\n");
-       String[] resultados = new String[] {"$300"};
+       String[] resultados = new String[] {"$200"};
        Assert.assertTrue(arr[arr.length-1].contains(resultados[0]));
     }
 	
 	@Test
-    public void IP_002(){
-        InputStream stdin = System.in;
-        System.setIn(new ByteArrayInputStream("45\nF\nY\n1234567890\n".getBytes()));
+    public void BC002(){
+       InputStream stdin = System.in;
+       System.setIn(new ByteArrayInputStream("85\n".getBytes()));
 
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        PrintStream ps = new PrintStream(byteArrayOutputStream);
-        PrintStream stdout = System.out;
-        System.setOut(ps);
+       ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+       PrintStream ps = new PrintStream(byteArrayOutputStream);
+       PrintStream stdout = System.out;
+       System.setOut(ps);
 
-        CarInsurance.main(new String[0]);
+       CarInsurance.main(new String[0]);
 
-        System.setIn(stdin);
-        System.setOut(stdout);
+       System.setIn(stdin);
+       System.setOut(stdout);
 
-        String outputText = byteArrayOutputStream.toString();
-        String[] arr = outputText.split("\n");
-        String[] resultados = new String[] {"$200"};
-        Assert.assertTrue(arr[arr.length-1].contains(resultados[0]));
-     }
-    
-	@Test
-    public void IP_003(){
-        InputStream stdin = System.in;
-        System.setIn(new ByteArrayInputStream("64\nM\nN\n1234567890\n".getBytes()));
-
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        PrintStream ps = new PrintStream(byteArrayOutputStream);
-        PrintStream stdout = System.out;
-        System.setOut(ps);
-
-        CarInsurance.main(new String[0]);
-
-        System.setIn(stdin);
-        System.setOut(stdout);
-
-        String outputText = byteArrayOutputStream.toString();
-        String[] arr = outputText.split("\n");
-        String[] resultados = new String[] {"$400"};
-        Assert.assertTrue(arr[arr.length-1].contains(resultados[0]));
-     }
-    
-	@Test
-    public void IP_004(){
-        InputStream stdin = System.in;
-        System.setIn(new ByteArrayInputStream("18\nM\nN\n1234567890\n".getBytes()));
-
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        PrintStream ps = new PrintStream(byteArrayOutputStream);
-        PrintStream stdout = System.out;
-        System.setOut(ps);
-
-        CarInsurance.main(new String[0]);
-
-        System.setIn(stdin);
-        System.setOut(stdout);
-
-        String outputText = byteArrayOutputStream.toString();
-        String[] arr = outputText.split("\n");
-        String[] resultados = new String[] {"$2000"};
-        Assert.assertTrue(arr[arr.length-1].contains(resultados[0]));
-     }
+       String outputText = byteArrayOutputStream.toString();
+       String[] arr = outputText.split("\n");
+       String[] resultados = new String[] {"Sorry, premium car insurance is not available for 80 years older"};
+       Assert.assertTrue(arr[arr.length-1].contains(resultados[0]));
+    }
 	
 	@Test
-    public void IP_005(){
-        InputStream stdin = System.in;
-        System.setIn(new ByteArrayInputStream("21\nF\nY\n123456789\n1234567890\n".getBytes()));
+    public void BC003(){
+       InputStream stdin = System.in;
+       System.setIn(new ByteArrayInputStream("15\n".getBytes()));
 
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        PrintStream ps = new PrintStream(byteArrayOutputStream);
-        PrintStream stdout = System.out;
-        System.setOut(ps);
+       ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+       PrintStream ps = new PrintStream(byteArrayOutputStream);
+       PrintStream stdout = System.out;
+       System.setOut(ps);
 
-        CarInsurance.main(new String[0]);
+       CarInsurance.main(new String[0]);
 
-        System.setIn(stdin);
-        System.setOut(stdout);
+       System.setIn(stdin);
+       System.setOut(stdout);
 
-        String outputText = byteArrayOutputStream.toString();
-        String[] arr = outputText.split("\n");
-        String[] resultados = new String[] {"$300"};
-        Assert.assertTrue(arr[arr.length-1].contains(resultados[0]));
-     }
+       String outputText = byteArrayOutputStream.toString();
+       String[] arr = outputText.split("\n");
+       String[] resultados = new String[] {"Sorry, premium car insurance is not available for under age"};
+       Assert.assertTrue(arr[arr.length-1].contains(resultados[0]));
+    }
 	
 	@Test
-    public void IP_006(){
-        InputStream stdin = System.in;
-        System.setIn(new ByteArrayInputStream("21\nF\nM\nY\n1234567890\n".getBytes()));
+    public void BC004(){
+	      InputStream stdin = System.in;
+	       System.setIn(new ByteArrayInputStream("24\nM\nN\n1234567891\n".getBytes()));
 
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        PrintStream ps = new PrintStream(byteArrayOutputStream);
-        PrintStream stdout = System.out;
-        System.setOut(ps);
+	       ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+	       PrintStream ps = new PrintStream(byteArrayOutputStream);
+	       PrintStream stdout = System.out;
+	       System.setOut(ps);
 
-        CarInsurance.main(new String[0]);
+	       CarInsurance.main(new String[0]);
 
-        System.setIn(stdin);
-        System.setOut(stdout);
+	       System.setIn(stdin);
+	       System.setOut(stdout);
 
-        String outputText = byteArrayOutputStream.toString();
-        String[] arr = outputText.split("\n");
-        String[] resultados = new String[] {"$300"};
-        Assert.assertTrue(arr[arr.length-1].contains(resultados[0]));
-     }
-	
+	       String outputText = byteArrayOutputStream.toString();
+	       String[] arr = outputText.split("\n");
+	       String[] resultados = new String[] {"$2000"};
+	       Assert.assertTrue(arr[arr.length-1].contains(resultados[0]));
+    }
 	@Test
-    public void IP_007(){
-        InputStream stdin = System.in;
-        System.setIn(new ByteArrayInputStream("21\nwoman\nF\nY\n1234567890\n".getBytes()));
+    public void BC005(){
+       InputStream stdin = System.in;
+       System.setIn(new ByteArrayInputStream("50\nM\nY\n1234567891\n".getBytes()));
 
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        PrintStream ps = new PrintStream(byteArrayOutputStream);
-        PrintStream stdout = System.out;
-        System.setOut(ps);
+       ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+       PrintStream ps = new PrintStream(byteArrayOutputStream);
+       PrintStream stdout = System.out;
+       System.setOut(ps);
 
-        CarInsurance.main(new String[0]);
+       CarInsurance.main(new String[0]);
 
-        System.setIn(stdin);
-        System.setOut(stdout);
+       System.setIn(stdin);
+       System.setOut(stdout);
 
-        String outputText = byteArrayOutputStream.toString();
-        String[] arr = outputText.split("\n");
-        String[] resultados = new String[] {"$300"};
-        Assert.assertTrue(arr[arr.length-1].contains(resultados[0]));
-     }
-	
-	
-	@Test
-    public void IP_008(){
-        InputStream stdin = System.in;
-        System.setIn(new ByteArrayInputStream("17\n".getBytes()));
-
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        PrintStream ps = new PrintStream(byteArrayOutputStream);
-        PrintStream stdout = System.out;
-        System.setOut(ps);
-
-        CarInsurance.main(new String[0]);
-
-        System.setIn(stdin);
-        System.setOut(stdout);
-
-        String outputText = byteArrayOutputStream.toString();
-        String[] arr = outputText.split("\n");
-        String[] resultados = new String[] {"Sorry, premium car insurance is not available for under age"};
-        Assert.assertTrue(arr[arr.length-1].contains(resultados[0]));
-     }
-	
-	@Test
-    public void IP_009(){
-        InputStream stdin = System.in;
-        System.setIn(new ByteArrayInputStream("81\n".getBytes()));
-
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        PrintStream ps = new PrintStream(byteArrayOutputStream);
-        PrintStream stdout = System.out;
-        System.setOut(ps);
-
-        CarInsurance.main(new String[0]);
-
-        System.setIn(stdin);
-        System.setOut(stdout);
-
-        String outputText = byteArrayOutputStream.toString();
-        String[] arr = outputText.split("\n");
-        String[] resultados = new String[] {"Sorry, premium car insurance is not available for 80 years older"};
-        Assert.assertTrue(arr[arr.length-1].contains(resultados[0]));
-     }
-	
-	@Test
-    public void IP_010(){
-        InputStream stdin = System.in;
-        System.setIn(new ByteArrayInputStream("twenty-one\n21\nF\nY\n1234567890\n".getBytes()));
-
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        PrintStream ps = new PrintStream(byteArrayOutputStream);
-        PrintStream stdout = System.out;
-        System.setOut(ps);
-
-        CarInsurance.main(new String[0]);
-
-        System.setIn(stdin);
-        System.setOut(stdout);
-
-        String outputText = byteArrayOutputStream.toString();
-        String[] arr = outputText.split("\n");
-        String[] resultados = new String[] {"$300"};
-        Assert.assertTrue(arr[arr.length-1].contains(resultados[0]));
-     }
+       String outputText = byteArrayOutputStream.toString();
+       String[] arr = outputText.split("\n");
+       String[] resultados = new String[] {"$200"};
+       Assert.assertTrue(arr[arr.length-1].contains(resultados[0]));
+    }
 }
+
--- ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/StatementCoverageTest.java
+++ ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/StatementCoverageTest.java
@@ -5,11 +5,11 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
-public class BranchCoverageTest {
+public class StatementCoverageTest {
 	@Test
-    public void BC001(){
+    public void pathTest1(){
        InputStream stdin = System.in;
-       System.setIn(new ByteArrayInputStream("none\n48\nund\nF\n*\nY\nnum\n1234567891\n".getBytes()));
+       System.setIn(new ByteArrayInputStream("20\nM\nN\n1234567891\n".getBytes()));
 
        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
        PrintStream ps = new PrintStream(byteArrayOutputStream);
@@ -23,14 +23,17 @@ public class BranchCoverageTest {
 
        String outputText = byteArrayOutputStream.toString();
        String[] arr = outputText.split("\n");
-       String[] resultados = new String[] {"$200"};
-       Assert.assertTrue(arr[arr.length-1].contains(resultados[0]));
+       String[] resultados = new String[] {"$2000\n"};
+       //System.out.print(arr[arr.length-1].getClass().getName());
+       //System.out.print(resultados[0].getClass().getName());
+       //String key = "output:";
+       //String output = outputText.substring(outputText.indexOf(key) + key.length()).trim();
+       Assert.assertEquals(arr[arr.length-1], resultados[0] );
     }
-	
 	@Test
-    public void BC002(){
+    public void pathTest2(){
        InputStream stdin = System.in;
-       System.setIn(new ByteArrayInputStream("85\n".getBytes()));
+       System.setIn(new ByteArrayInputStream("hola\n48\nchao\nF\nnegativo\nY\nnum\n1234567891\n".getBytes()));
 
        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
        PrintStream ps = new PrintStream(byteArrayOutputStream);
@@ -44,14 +47,17 @@ public class BranchCoverageTest {
 
        String outputText = byteArrayOutputStream.toString();
        String[] arr = outputText.split("\n");
-       String[] resultados = new String[] {"Sorry, premium car insurance is not available for 80 years older"};
-       Assert.assertTrue(arr[arr.length-1].contains(resultados[0]));
+       String[] resultados = new String[] {"$200\n"};
+       //System.out.print(arr[arr.length-1].getClass().getName());
+       //System.out.print(resultados[0].getClass().getName());
+       //String key = "output:";
+       //String output = outputText.substring(outputText.indexOf(key) + key.length()).trim();
+       Assert.assertEquals(arr[arr.length-1], resultados[0] );
     }
-	
 	@Test
-    public void BC003(){
+    public void pathTest3(){
        InputStream stdin = System.in;
-       System.setIn(new ByteArrayInputStream("15\n".getBytes()));
+       System.setIn(new ByteArrayInputStream("10\n".getBytes()));
 
        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
        PrintStream ps = new PrintStream(byteArrayOutputStream);
@@ -65,34 +71,17 @@ public class BranchCoverageTest {
 
        String outputText = byteArrayOutputStream.toString();
        String[] arr = outputText.split("\n");
-       String[] resultados = new String[] {"Sorry, premium car insurance is not available for under age"};
-       Assert.assertTrue(arr[arr.length-1].contains(resultados[0]));
-    }
-	
-	@Test
-    public void BC004(){
-	      InputStream stdin = System.in;
-	       System.setIn(new ByteArrayInputStream("24\nM\nN\n1234567891\n".getBytes()));
-
-	       ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-	       PrintStream ps = new PrintStream(byteArrayOutputStream);
-	       PrintStream stdout = System.out;
-	       System.setOut(ps);
-
-	       CarInsurance.main(new String[0]);
-
-	       System.setIn(stdin);
-	       System.setOut(stdout);
-
-	       String outputText = byteArrayOutputStream.toString();
-	       String[] arr = outputText.split("\n");
-	       String[] resultados = new String[] {"$2000"};
-	       Assert.assertTrue(arr[arr.length-1].contains(resultados[0]));
+       String[] resultados = new String[] {"Sorry, premium car insurance is not available for under age\n"};
+       //System.out.print(arr[arr.length-1].getClass().getName());
+       //System.out.print(resultados[0].getClass().getName());
+       //String key = "output:";
+       //String output = outputText.substring(outputText.indexOf(key) + key.length()).trim();
+       Assert.assertEquals(arr[arr.length-1], resultados[0] );
     }
 	@Test
-    public void BC005(){
+    public void pathTest4(){
        InputStream stdin = System.in;
-       System.setIn(new ByteArrayInputStream("50\nM\nY\n1234567891\n".getBytes()));
+       System.setIn(new ByteArrayInputStream("90\n".getBytes()));
 
        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
        PrintStream ps = new PrintStream(byteArrayOutputStream);
@@ -106,7 +95,11 @@ public class BranchCoverageTest {
 
        String outputText = byteArrayOutputStream.toString();
        String[] arr = outputText.split("\n");
-       String[] resultados = new String[] {"$200"};
-       Assert.assertTrue(arr[arr.length-1].contains(resultados[0]));
+       String[] resultados = new String[] {"Sorry, premium car insurance is not available for 80 years older\n"};
+       //System.out.print(arr[arr.length-1].getClass().getName());
+       //System.out.print(resultados[0].getClass().getName());
+       //String key = "output:";
+       //String output = outputText.substring(outputText.indexOf(key) + key.length()).trim();
+       Assert.assertEquals(arr[arr.length-1], resultados[0] );
     }
 }
